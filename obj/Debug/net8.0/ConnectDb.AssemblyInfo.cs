e18149c65bb8be0ffc7235c3a73ffea473c864bc
My Name <srivastavaadi129@gmail.com>
2025-03-26 12:14:31 +0530

    added DockerFile

--- obj/Debug/net8.0/ConnectDb.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConnectDb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectDb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5183372ef06673e139cc4c1724110c86420e11a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f916451df0328c451c47d1fd9a57e6cfdd6e73f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectDb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectDb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
